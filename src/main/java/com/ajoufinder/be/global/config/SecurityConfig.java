4abae42e3ff22b0812bb6793854f46b12d57e4cf
Ina (PC) <shlovejo2@ajou.ac.kr>
2025-05-16 18:59:23 +0900

    [FEAT] 비밀번호 인코딩하는 passwordEncoder 빈 추가

--- src/main/java/com/ajoufinder/be/global/config/SecurityConfig.java
+++ src/main/java/com/ajoufinder/be/global/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.ajoufinder.be.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +21,10 @@ public class SecurityConfig {
                 .httpBasic(basic -> basic.disable()); // HTTP Basic 인증 비활성화
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
