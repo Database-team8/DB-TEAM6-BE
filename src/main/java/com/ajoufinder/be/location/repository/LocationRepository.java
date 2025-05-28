6339128dc982b91f0b3005a6a3586b1725958bb3
Ina (PC) <shlovejo2@ajou.ac.kr>
2025-05-28 22:26:12 +0900

    [FIX] LocationRepository 상속받는 인터페이스 JpaRepository로 수정

--- src/main/java/com/ajoufinder/be/location/repository/LocationRepository.java
+++ src/main/java/com/ajoufinder/be/location/repository/LocationRepository.java
@@ -1,9 +1,12 @@
 package com.ajoufinder.be.location.repository;
 
 import com.ajoufinder.be.location.domain.Location;
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LocationRepository extends CrudRepository<Location, Long> {
+public interface LocationRepository extends JpaRepository<Location, Long> {
+    @Override
+    List<Location> findAll();
 }
