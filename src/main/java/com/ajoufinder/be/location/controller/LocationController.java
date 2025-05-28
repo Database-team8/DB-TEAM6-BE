35755d0fd549b6dfdfb68423628fd9c75e38cac7
Ina (PC) <shlovejo2@ajou.ac.kr>
2025-05-28 22:26:44 +0900

    [FEAT] 위치 정보리스트 조회 API 개발

--- src/main/java/com/ajoufinder/be/location/controller/LocationController.java
+++ src/main/java/com/ajoufinder/be/location/controller/LocationController.java
@@ -1,8 +1,14 @@
 package com.ajoufinder.be.location.controller;
 
+import com.ajoufinder.be.global.api_response.ApiResponse;
+import com.ajoufinder.be.location.dto.response.LocationResponse;
+import com.ajoufinder.be.location.service.LocationService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/location")
+@RequestMapping("/locations")
 public class LocationController {
+    private final LocationService locationService;
+
+    @Operation(
+            summary = "모든 위치 조회",
+            description = "등록된 모든 위치 정보를 조회합니다."
+    )
+    @GetMapping
+    public ApiResponse<List<LocationResponse>> getAllLocations() {
+        List<LocationResponse> locations = locationService.getAllLocations();
+        return ApiResponse.onSuccess(locations);
+    }
 }
