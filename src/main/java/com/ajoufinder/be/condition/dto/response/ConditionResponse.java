3d99074dae8e600c3e8e1ec38aea9c7d573c5a80
Ina (PC) <shlovejo2@ajou.ac.kr>
2025-06-02 18:03:52 +0900

    [FEAT] 조건 리스트 조회, 조건 생성 & 삭제 기능 구현

---
+++ src/main/java/com/ajoufinder/be/condition/dto/response/ConditionResponse.java
@@ -0,0 +1,21 @@
+package com.ajoufinder.be.condition.dto.response;
+
+import com.ajoufinder.be.condition.domain.Condition;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ConditionResponse {
+    private Long conditionId;
+    private String itemType;
+    private String location;
+
+    public static ConditionResponse from(Condition condition) {
+        return new ConditionResponse(
+                condition.getId(),
+                condition.getItemType().getItemType().name(),
+                condition.getLocation().getLocationName().name()
+        );
+    }
+}
