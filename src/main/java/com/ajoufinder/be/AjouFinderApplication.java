53dd7e1b75771ef525b76c4f3d516c1712951639
Ina (PC) <shlovejo2@ajou.ac.kr>
2025-06-04 10:39:40 +0900

    [REFACTOR] EnableJpaAuditing 애노테이션 추가

--- src/main/java/com/ajoufinder/be/AjouFinderApplication.java
+++ src/main/java/com/ajoufinder/be/AjouFinderApplication.java
@@ -2,8 +2,10 @@ package com.ajoufinder.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AjouFinderApplication {
 
     public static void main(String[] args) {
