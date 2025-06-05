15a58bf5616b48331e77b6a4d668fb6a2ba6363c
Ina (PC) <shlovejo2@ajou.ac.kr>
2025-06-05 17:09:11 +0900

    [FEAT] 사용자 조건 생성 API 구현

--- src/main/java/com/ajoufinder/be/condition/dto/request/ConditionCreateRequest.java
+++ src/main/java/com/ajoufinder/be/condition/dto/request/ConditionCreateRequest.java
@@ -8,16 +8,16 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Schema(description = "조건 생성 요청 DTO")
 public record ConditionCreateRequest(
-        @NotBlank(message = "아이템 타입을 선택해주세요.")
+        @NotNull(message = "아이템 타입을 선택해주세요.")
         @Schema(description = "조건을 적용할 아이템 타입", example = "1")
         Long itemTypeId,
 
-        @NotBlank(message = "위치를 선택해주세요.")
+        @NotNull(message = "위치를 선택해주세요.")
         @Schema(description = "조건을 적용할 위치", example = "4")
         Long locationId
 ){
